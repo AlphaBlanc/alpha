0778b3d82d9811a90c81c0e6b1ad07cbfc8ca8cf
AlphaBlanc <67218751+AlphaBlanc@users.noreply.github.com>
2020-06-21 17:50:17 +0900

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -9,9 +9,8 @@ async def on_ready():
     print("다음으로 로그인합니다 : ")
     print(client.user.name)
     print(client.user.id)
-    print("==========")
-    game = discord.Game("서버주소: Alpha.minesv.kr")
-    await app.change_presence(status=discord.Status.online, activity=game)
+    print("------------------")
+    await bot.change_presence(game=discord.Game(name="서버주소: Alpha.minesv.kr", type=1))
 
 access_token = os.environ["token"]
 client.run(access_token)
