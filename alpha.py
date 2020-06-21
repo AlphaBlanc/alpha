a4667d572af68963249f2589fcc243631931abe3
AlphaBlanc <67218751+AlphaBlanc@users.noreply.github.com>
2020-06-21 17:53:44 +0900

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -10,7 +10,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------------------")
-    await client.change_presence(game=discord.Game(name="서버주소: Alpha.minesv.kr", type=1))
+    await client.change_presence(game=discord.Game(name="Alpha.minesv.kr", type=1))
 
 access_token = os.environ["token"]
 client.run(access_token)
