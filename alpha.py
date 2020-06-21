e6dc00aaa487d7b4e678f9d9996fdafc160b28e0
AlphaBlanc <67218751+AlphaBlanc@users.noreply.github.com>
2020-06-21 17:48:56 +0900

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -7,7 +7,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("다음으로 로그인합니다 : ")
-    print(clientclient.user.name)
+    print(client.user.name)
     print(client.user.id)
     print("==========")
     game = discord.Game("서버주소: Alpha.minesv.kr")
